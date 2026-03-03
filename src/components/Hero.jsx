3a41a3853576106417bdef2dfaa031bf9d7b46ea
sudheeesh <sudheeshvilla5@gmail.com>
2026-03-03 21:33:54 +0530

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,16 @@ import './Hero.css';
 const Hero = () => {
     return (
         <header className="hero">
-            <iframe src="https://drive.google.com/file/d/11HvfOQyZ94PSX96wbQ82lkqwq7jVpJE3/view?usp=drive_link" width="100%" height="100%" allow="autoplay" className="hero-video"></iframe>
+            {/* Google Drive iframe instead of video tag */}
+            <iframe
+                src="https://drive.google.com/file/d/11HvfOQyZ94PSX96wbQ82lkqwq7jVpJE3/view?usp=drive_link"
+                width="100%"
+                height="100%"
+                className="hero-video"
+                title="Toni and Guy Video"
+                allow="autoplay; fullscreen"
+                style={{ border: 'none' }}
+            ></iframe>
             <div className="hero-overlay"></div>
             <div className="container hero-content animate-fade-in">
                 <h1 className="hero-title">
